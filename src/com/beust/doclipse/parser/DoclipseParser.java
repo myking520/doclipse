5eda402202393612c629687220c08b520adda736
kongguoan <kongguoan@isphere.top>
2016-01-25 17:13:48 +0800

    取消antlr依赖

--- src/com/beust/doclipse/parser/DoclipseParser.java
+++ src/com/beust/doclipse/parser/DoclipseParser.java
@@ -1,268 +1,268 @@
-// $ANTLR 2.7.3: "src/doclipse.g" -> "DoclipseParser.java"$
-
-package com.beust.doclipse.parser;
-import java.io.StringReader;
-
-import antlr.TokenBuffer;
-import antlr.TokenStreamException;
-import antlr.TokenStreamIOException;
-import antlr.ANTLRException;
-import antlr.LLkParser;
-import antlr.Token;
-import antlr.TokenStream;
-import antlr.RecognitionException;
-import antlr.NoViableAltException;
-import antlr.MismatchedTokenException;
-import antlr.SemanticException;
-import antlr.ParserSharedInputState;
-import antlr.collections.impl.BitSet;
-
-public class DoclipseParser extends antlr.LLkParser       implements DoclipseParserTokenTypes
- {
-
-
-    static public boolean completeOnTagName = true;
-    static public boolean completeOnAttributeName = false;
-    static public boolean completeOnAttributeValue = false;
-    static public Token fragment;
-    private String identifier = null;
-
-    static public void main(String[] argv)  
-      throws antlr.RecognitionException, antlr.TokenStreamException
-    {
-      String s = "@ej a = b";
-      if (argv.length > 0) {
-        s = argv[0];
-      }
-      System.out.println("Parsing " + s);
-      
-      {
-	      StringReader sr = new StringReader(s);
-	      DoclipseLexer dl = new DoclipseLexer(sr);
-	      Token t = dl.nextToken();
-	      while (t.getType() != Token.EOF_TYPE) {
-	        System.out.println("Token: " + t.getText());
-	        t = dl.nextToken();
-	      }
-	    }
-	      /*
-	      DoclipseParser parser = new DoclipseParser(dl);
-	      parser.start();
-	      System.out.println("Parsing successful @" + fragment.toString() + "@"
-	       + " " + completeOnTagName
-	       + " " + completeOnAttributeName
-	       + " " + completeOnAttributeValue);
-	     }
-	     
-	     {
-	      StringReader sr = new StringReader(s);
-	      DoclipseLexer dl = new DoclipseLexer(sr);
-	      DoclipseParser parser = new DoclipseParser(dl);
-	      parser.start();
-	      System.out.println("Parsing successful @" + fragment.toString() + "@"
-	         + " " + completeOnTagName
-	         + " " + completeOnAttributeName
-	         + " " + completeOnAttributeValue);
-	      }
-	      */
-	    }
-    
-protected DoclipseParser(TokenBuffer tokenBuf, int k) {
-  super(tokenBuf,k);
-  tokenNames = _tokenNames;
-}
-
-public DoclipseParser(TokenBuffer tokenBuf) {
-  this(tokenBuf,1);
-}
-
-protected DoclipseParser(TokenStream lexer, int k) {
-  super(lexer,k);
-  tokenNames = _tokenNames;
-}
-
-public DoclipseParser(TokenStream lexer) {
-  this(lexer,1);
-}
-
-public DoclipseParser(ParserSharedInputState state) {
-  super(state,1);
-  tokenNames = _tokenNames;
-}
-
-	public final void start() throws RecognitionException, TokenStreamException {
-		
-		Token  tag = null;
-		
-		try {      // for error handling
-			if ((LA(1)==TAG)) {
-				match(TAG);
-				{
-				int _cnt3=0;
-				_loop3:
-				do {
-					if ((LA(1)==IDENTIFIER)) {
-						completeAssignment();
-					}
-					else {
-						if ( _cnt3>=1 ) { break _loop3; } else {throw new NoViableAltException(LT(1), getFilename());}
-					}
-					
-					_cnt3++;
-				} while (true);
-				}
-				{
-				switch ( LA(1)) {
-				case IDENTIFIER:
-				{
-					partialAssignment();
-					break;
-				}
-				case EOF:
-				{
-					break;
-				}
-				default:
-				{
-					throw new NoViableAltException(LT(1), getFilename());
-				}
-				}
-				}
-			}
-			else if ((LA(1)==TAG)) {
-				tag = LT(1);
-				match(TAG);
-				{
-				switch ( LA(1)) {
-				case IDENTIFIER:
-				{
-					partialAssignment();
-					break;
-				}
-				case EOF:
-				{
-					break;
-				}
-				default:
-				{
-					throw new NoViableAltException(LT(1), getFilename());
-				}
-				}
-				}
-				System.out.println("TAG :" + tag);
-			}
-			else {
-				throw new NoViableAltException(LT(1), getFilename());
-			}
-			
-		}
-		catch (RecognitionException ex) {
-			reportError(ex);
-			consume();
-			consumeUntil(_tokenSet_0);
-		}
-	}
-	
-	public final void completeAssignment() throws RecognitionException, TokenStreamException {
-		
-		
-		try {      // for error handling
-			match(IDENTIFIER);
-			match(EQ);
-			value();
-			System.out.println("COMPLETE");
-		}
-		catch (RecognitionException ex) {
-			reportError(ex);
-			consume();
-			consumeUntil(_tokenSet_1);
-		}
-	}
-	
-	public final void partialAssignment() throws RecognitionException, TokenStreamException {
-		
-		
-		try {      // for error handling
-			match(IDENTIFIER);
-			{
-			switch ( LA(1)) {
-			case EQ:
-			{
-				match(EQ);
-				break;
-			}
-			case EOF:
-			{
-				break;
-			}
-			default:
-			{
-				throw new NoViableAltException(LT(1), getFilename());
-			}
-			}
-			}
-			System.out.println("PARTIAL 1");
-		}
-		catch (RecognitionException ex) {
-			reportError(ex);
-			consume();
-			consumeUntil(_tokenSet_0);
-		}
-	}
-	
-	public final void value() throws RecognitionException, TokenStreamException {
-		
-		
-		try {      // for error handling
-			switch ( LA(1)) {
-			case IDENTIFIER:
-			{
-				match(IDENTIFIER);
-				break;
-			}
-			case 7:
-			{
-				match(7);
-				match(IDENTIFIER);
-				match(7);
-				break;
-			}
-			default:
-			{
-				throw new NoViableAltException(LT(1), getFilename());
-			}
-			}
-		}
-		catch (RecognitionException ex) {
-			reportError(ex);
-			consume();
-			consumeUntil(_tokenSet_1);
-		}
-	}
-	
-	
-	public static final String[] _tokenNames = {
-		"<0>",
-		"EOF",
-		"<2>",
-		"NULL_TREE_LOOKAHEAD",
-		"TAG",
-		"IDENTIFIER",
-		"EQ",
-		"\"\\\"\"",
-		"DQ",
-		"WS"
-	};
-	
-	private static final long[] mk_tokenSet_0() {
-		long[] data = { 2L, 0L};
-		return data;
-	}
-	public static final BitSet _tokenSet_0 = new BitSet(mk_tokenSet_0());
-	private static final long[] mk_tokenSet_1() {
-		long[] data = { 34L, 0L};
-		return data;
-	}
-	public static final BitSet _tokenSet_1 = new BitSet(mk_tokenSet_1());
-	
-	}
+//// $ANTLR 2.7.3: "src/doclipse.g" -> "DoclipseParser.java"$
+//
+//package com.beust.doclipse.parser;
+//import java.io.StringReader;
+//
+//import antlr.TokenBuffer;
+//import antlr.TokenStreamException;
+//import antlr.TokenStreamIOException;
+//import antlr.ANTLRException;
+//import antlr.LLkParser;
+//import antlr.Token;
+//import antlr.TokenStream;
+//import antlr.RecognitionException;
+//import antlr.NoViableAltException;
+//import antlr.MismatchedTokenException;
+//import antlr.SemanticException;
+//import antlr.ParserSharedInputState;
+//import antlr.collections.impl.BitSet;
+//
+//public class DoclipseParser extends antlr.LLkParser       implements DoclipseParserTokenTypes
+// {
+//
+//
+//    static public boolean completeOnTagName = true;
+//    static public boolean completeOnAttributeName = false;
+//    static public boolean completeOnAttributeValue = false;
+//    static public Token fragment;
+//    private String identifier = null;
+//
+//    static public void main(String[] argv)  
+//      throws antlr.RecognitionException, antlr.TokenStreamException
+//    {
+//      String s = "@ej a = b";
+//      if (argv.length > 0) {
+//        s = argv[0];
+//      }
+//      System.out.println("Parsing " + s);
+//      
+//      {
+//	      StringReader sr = new StringReader(s);
+//	      DoclipseLexer dl = new DoclipseLexer(sr);
+//	      Token t = dl.nextToken();
+//	      while (t.getType() != Token.EOF_TYPE) {
+//	        System.out.println("Token: " + t.getText());
+//	        t = dl.nextToken();
+//	      }
+//	    }
+//	      /*
+//	      DoclipseParser parser = new DoclipseParser(dl);
+//	      parser.start();
+//	      System.out.println("Parsing successful @" + fragment.toString() + "@"
+//	       + " " + completeOnTagName
+//	       + " " + completeOnAttributeName
+//	       + " " + completeOnAttributeValue);
+//	     }
+//	     
+//	     {
+//	      StringReader sr = new StringReader(s);
+//	      DoclipseLexer dl = new DoclipseLexer(sr);
+//	      DoclipseParser parser = new DoclipseParser(dl);
+//	      parser.start();
+//	      System.out.println("Parsing successful @" + fragment.toString() + "@"
+//	         + " " + completeOnTagName
+//	         + " " + completeOnAttributeName
+//	         + " " + completeOnAttributeValue);
+//	      }
+//	      */
+//	    }
+//    
+//protected DoclipseParser(TokenBuffer tokenBuf, int k) {
+//  super(tokenBuf,k);
+//  tokenNames = _tokenNames;
+//}
+//
+//public DoclipseParser(TokenBuffer tokenBuf) {
+//  this(tokenBuf,1);
+//}
+//
+//protected DoclipseParser(TokenStream lexer, int k) {
+//  super(lexer,k);
+//  tokenNames = _tokenNames;
+//}
+//
+//public DoclipseParser(TokenStream lexer) {
+//  this(lexer,1);
+//}
+//
+//public DoclipseParser(ParserSharedInputState state) {
+//  super(state,1);
+//  tokenNames = _tokenNames;
+//}
+//
+//	public final void start() throws RecognitionException, TokenStreamException {
+//		
+//		Token  tag = null;
+//		
+//		try {      // for error handling
+//			if ((LA(1)==TAG)) {
+//				match(TAG);
+//				{
+//				int _cnt3=0;
+//				_loop3:
+//				do {
+//					if ((LA(1)==IDENTIFIER)) {
+//						completeAssignment();
+//					}
+//					else {
+//						if ( _cnt3>=1 ) { break _loop3; } else {throw new NoViableAltException(LT(1), getFilename());}
+//					}
+//					
+//					_cnt3++;
+//				} while (true);
+//				}
+//				{
+//				switch ( LA(1)) {
+//				case IDENTIFIER:
+//				{
+//					partialAssignment();
+//					break;
+//				}
+//				case EOF:
+//				{
+//					break;
+//				}
+//				default:
+//				{
+//					throw new NoViableAltException(LT(1), getFilename());
+//				}
+//				}
+//				}
+//			}
+//			else if ((LA(1)==TAG)) {
+//				tag = LT(1);
+//				match(TAG);
+//				{
+//				switch ( LA(1)) {
+//				case IDENTIFIER:
+//				{
+//					partialAssignment();
+//					break;
+//				}
+//				case EOF:
+//				{
+//					break;
+//				}
+//				default:
+//				{
+//					throw new NoViableAltException(LT(1), getFilename());
+//				}
+//				}
+//				}
+//				System.out.println("TAG :" + tag);
+//			}
+//			else {
+//				throw new NoViableAltException(LT(1), getFilename());
+//			}
+//			
+//		}
+//		catch (RecognitionException ex) {
+//			reportError(ex);
+//			consume();
+//			consumeUntil(_tokenSet_0);
+//		}
+//	}
+//	
+//	public final void completeAssignment() throws RecognitionException, TokenStreamException {
+//		
+//		
+//		try {      // for error handling
+//			match(IDENTIFIER);
+//			match(EQ);
+//			value();
+//			System.out.println("COMPLETE");
+//		}
+//		catch (RecognitionException ex) {
+//			reportError(ex);
+//			consume();
+//			consumeUntil(_tokenSet_1);
+//		}
+//	}
+//	
+//	public final void partialAssignment() throws RecognitionException, TokenStreamException {
+//		
+//		
+//		try {      // for error handling
+//			match(IDENTIFIER);
+//			{
+//			switch ( LA(1)) {
+//			case EQ:
+//			{
+//				match(EQ);
+//				break;
+//			}
+//			case EOF:
+//			{
+//				break;
+//			}
+//			default:
+//			{
+//				throw new NoViableAltException(LT(1), getFilename());
+//			}
+//			}
+//			}
+//			System.out.println("PARTIAL 1");
+//		}
+//		catch (RecognitionException ex) {
+//			reportError(ex);
+//			consume();
+//			consumeUntil(_tokenSet_0);
+//		}
+//	}
+//	
+//	public final void value() throws RecognitionException, TokenStreamException {
+//		
+//		
+//		try {      // for error handling
+//			switch ( LA(1)) {
+//			case IDENTIFIER:
+//			{
+//				match(IDENTIFIER);
+//				break;
+//			}
+//			case 7:
+//			{
+//				match(7);
+//				match(IDENTIFIER);
+//				match(7);
+//				break;
+//			}
+//			default:
+//			{
+//				throw new NoViableAltException(LT(1), getFilename());
+//			}
+//			}
+//		}
+//		catch (RecognitionException ex) {
+//			reportError(ex);
+//			consume();
+//			consumeUntil(_tokenSet_1);
+//		}
+//	}
+//	
+//	
+//	public static final String[] _tokenNames = {
+//		"<0>",
+//		"EOF",
+//		"<2>",
+//		"NULL_TREE_LOOKAHEAD",
+//		"TAG",
+//		"IDENTIFIER",
+//		"EQ",
+//		"\"\\\"\"",
+//		"DQ",
+//		"WS"
+//	};
+//	
+//	private static final long[] mk_tokenSet_0() {
+//		long[] data = { 2L, 0L};
+//		return data;
+//	}
+//	public static final BitSet _tokenSet_0 = new BitSet(mk_tokenSet_0());
+//	private static final long[] mk_tokenSet_1() {
+//		long[] data = { 34L, 0L};
+//		return data;
+//	}
+//	public static final BitSet _tokenSet_1 = new BitSet(mk_tokenSet_1());
+//	
+//	}
--- src/com/beust/doclipse/tag/TagFragmentAntlr.java
+++ src/com/beust/doclipse/tag/TagFragmentAntlr.java
@@ -1,205 +1,205 @@
-package com.beust.doclipse.tag;
-
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.List;
-
-import antlr.Token;
-import antlr.TokenStreamException;
-
-import com.beust.doclipse.parser.DoclipseLexer;
-
-/**
- * Parse tags with ANTLR.
- *
- * @author Cedric Beust, Jul 19, 2004
- * 
- */
-public class TagFragmentAntlr implements ITagFragment {
-	private String m_string = null;
-	private String m_attributeValue = null;
-	private boolean m_completesOnAttribute = false;
-	private boolean m_completesOnTagName = true;
-	private String m_tagName = null;
-	private String m_attributeName = null;
-
-	// The string we are trying to complete
-	private String m_fragment = "";
-
-	public TagFragmentAntlr(String s) {
-		m_string = s;
-		parseFragment();
-	}
-
-	private String nextToken() throws TokenStreamException {
-		Token t = m_lexer.nextToken();
-		StringBuffer sb = new StringBuffer();
-		boolean missEnd = false;
-		if ("\"".equals(t.getText())) {
-			t = m_lexer.nextToken();
-			String text = t.getText();
-			while (!"\"".equals(text) && text != null) {
-				sb.append(text);
-				t = m_lexer.nextToken();
-				text = t.getText();
-			}
-			missEnd = text == null;
-		} else {
-			sb.append(t.getText());
-		}
-		String result = sb.toString();
-		if (Token.EOF_TYPE == t.getType()) {
-			if (missEnd) {
-				//result = "\"";
-			} else {
-				result = null;
-			}
-		}
-		return result;
-	}
-
-	DoclipseLexer m_lexer = null;
-
-	private void parseFragment() {
-		StringReader sr = new StringReader(m_string);
-		m_lexer = new DoclipseLexer(sr);
-		try {
-			String t = nextToken();
-
-			if (null != t) {
-				m_tagName = t;
-				m_fragment = m_tagName;
-				t = nextToken();
-			}
-
-			while (null != t) {
-				m_attributeValue = null;
-				m_attributeName = t;
-				m_fragment = m_attributeName;
-				t = nextToken();
-				if (null != t) {
-					// should be an = sign, skip it
-					t = nextToken();
-					if (null != t) {
-						m_attributeValue = t;
-						m_fragment = m_attributeValue;
-						t = nextToken();
-					}
-				}
-			}
-		} catch (TokenStreamException ex) {
-			ex.printStackTrace();
-		}
-
-		//
-		// Parsing finished, set the booleans
-		//
-		if (m_string.length() > 0) {
-			int ind = m_string.length() - 1;
-			char c = m_string.charAt(ind);
-			boolean isSeparator = isSeparator(c);
-			if (isSeparator) {
-				m_fragment = "";
-			}
-
-			// @a b = c
-			if (null != m_tagName && null != m_attributeName && null != m_attributeValue) {
-				if (isSeparator) {
-					setCompletes(false, true);
-				} else {
-					setCompletes(false, false);
-				}
-			}
-			// @a b
-			else if (null != m_tagName && null != m_attributeName && null == m_attributeValue) {
-				if (isSeparator) {
-					setCompletes(false, false);
-				} else {
-					setCompletes(false, true);
-				}
-			}
-			// @a
-			else if (null != m_tagName && null == m_attributeName && null == m_attributeValue) {
-				if (isSeparator) {
-					setCompletes(false, true);
-				} else {
-					setCompletes(true, false);
-				}
-			}
-		}
-
-	}
-
-	private boolean isSeparator(char c) {
-		return isBlank(c) || c == '=';
-	}
-
-	static final private String BLANKS = "* \t\n\r";
-
-	private boolean isBlank(char c) {
-		return -1 != BLANKS.indexOf(c);
-	}
-
-	private void setCompletes(boolean tag, boolean attribute) {
-		m_completesOnTagName = tag;
-		m_completesOnAttribute = attribute;
-	}
-
-	public String getFragment() {
-		return m_fragment;
-	}
-
-	public String getTagName() {
-		return m_tagName;
-	}
-
-	public String getAttributeName() {
-		return m_attributeName;
-	}
-
-	public boolean completesOnTagName() {
-		return m_completesOnTagName;
-	}
-
-	public boolean completesOnAttribute() {
-		return m_completesOnAttribute;
-	}
-
-	private static void ppp(String s) {
-		System.out.println("[TagFragment] " + s);
-	}
-
-	public static void main(String[] argv) {
-		// String s = "a=b";
-		String[] strings = { "", "@ej", "@ej ", "@ej a", "@ej a ", "@ej a =", "@ej a = ", "@ej a = b", "@ej a = \"b\"",
-				"@ej a = b ", "@ej e = \"a b c\" ", "@ej\n  *  e = \"a b c\"\n  * " };
-
-		String[] fragments = { "", "@ej", "", "a", "", "", "", "b", "b", "", "", "" };
-
-		boolean[] completesOnAttribute = { false, false, true, true, false, false, false, false, false, true, true,
-				true };
-
-		boolean[] completesOnTagName = { true, true, false, false, false, false, false, false, false, false, false,
-				false };
-
-		String[] attributeNames = { "", "", "", "a", "a", "a", "a", "a", "a", "a", "e", "e" };
-
-		for (int i = 0; i < fragments.length; i++) {
-			ppp("TEST " + i);
-			ITagFragment tf = TagFragmentFactory.newTagFragment(strings[i]);
-			assert tf.completesOnTagName() == completesOnTagName[i] : i + " Complete on tagName : found "
-					+ tf.completesOnTagName() + " but expected " + completesOnTagName[i];
-			assert tf.getFragment().equals(fragments[i]) : i + " Wrong fragment : found '" + tf.getFragment()
-					+ "' but expected '" + fragments[i] + "'";
-			assert tf.completesOnAttribute() == completesOnAttribute[i] : i + " Complete on attribute : found "
-					+ tf.completesOnAttribute() + " but expected " + completesOnAttribute[i];
-
-			String an = tf.getAttributeName() == null ? "" : tf.getAttributeName();
-			assert an.equals(attributeNames[i]) : i + " Wrong attributeName : found '" + an + "' but expected '"
-					+ attributeNames[i] + "'";
-		}
-
-		ppp("ALL TESTS PASSED");
-	}
-
-}
+//package com.beust.doclipse.tag;
+//
+//import java.io.StringReader;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import antlr.Token;
+//import antlr.TokenStreamException;
+//
+//import com.beust.doclipse.parser.DoclipseLexer;
+//
+///**
+// * Parse tags with ANTLR.
+// *
+// * @author Cedric Beust, Jul 19, 2004
+// * 
+// */
+//public class TagFragmentAntlr implements ITagFragment {
+//	private String m_string = null;
+//	private String m_attributeValue = null;
+//	private boolean m_completesOnAttribute = false;
+//	private boolean m_completesOnTagName = true;
+//	private String m_tagName = null;
+//	private String m_attributeName = null;
+//
+//	// The string we are trying to complete
+//	private String m_fragment = "";
+//
+//	public TagFragmentAntlr(String s) {
+//		m_string = s;
+//		parseFragment();
+//	}
+//
+//	private String nextToken() throws TokenStreamException {
+//		Token t = m_lexer.nextToken();
+//		StringBuffer sb = new StringBuffer();
+//		boolean missEnd = false;
+//		if ("\"".equals(t.getText())) {
+//			t = m_lexer.nextToken();
+//			String text = t.getText();
+//			while (!"\"".equals(text) && text != null) {
+//				sb.append(text);
+//				t = m_lexer.nextToken();
+//				text = t.getText();
+//			}
+//			missEnd = text == null;
+//		} else {
+//			sb.append(t.getText());
+//		}
+//		String result = sb.toString();
+//		if (Token.EOF_TYPE == t.getType()) {
+//			if (missEnd) {
+//				//result = "\"";
+//			} else {
+//				result = null;
+//			}
+//		}
+//		return result;
+//	}
+//
+//	DoclipseLexer m_lexer = null;
+//
+//	private void parseFragment() {
+//		StringReader sr = new StringReader(m_string);
+//		m_lexer = new DoclipseLexer(sr);
+//		try {
+//			String t = nextToken();
+//
+//			if (null != t) {
+//				m_tagName = t;
+//				m_fragment = m_tagName;
+//				t = nextToken();
+//			}
+//
+//			while (null != t) {
+//				m_attributeValue = null;
+//				m_attributeName = t;
+//				m_fragment = m_attributeName;
+//				t = nextToken();
+//				if (null != t) {
+//					// should be an = sign, skip it
+//					t = nextToken();
+//					if (null != t) {
+//						m_attributeValue = t;
+//						m_fragment = m_attributeValue;
+//						t = nextToken();
+//					}
+//				}
+//			}
+//		} catch (TokenStreamException ex) {
+//			ex.printStackTrace();
+//		}
+//
+//		//
+//		// Parsing finished, set the booleans
+//		//
+//		if (m_string.length() > 0) {
+//			int ind = m_string.length() - 1;
+//			char c = m_string.charAt(ind);
+//			boolean isSeparator = isSeparator(c);
+//			if (isSeparator) {
+//				m_fragment = "";
+//			}
+//
+//			// @a b = c
+//			if (null != m_tagName && null != m_attributeName && null != m_attributeValue) {
+//				if (isSeparator) {
+//					setCompletes(false, true);
+//				} else {
+//					setCompletes(false, false);
+//				}
+//			}
+//			// @a b
+//			else if (null != m_tagName && null != m_attributeName && null == m_attributeValue) {
+//				if (isSeparator) {
+//					setCompletes(false, false);
+//				} else {
+//					setCompletes(false, true);
+//				}
+//			}
+//			// @a
+//			else if (null != m_tagName && null == m_attributeName && null == m_attributeValue) {
+//				if (isSeparator) {
+//					setCompletes(false, true);
+//				} else {
+//					setCompletes(true, false);
+//				}
+//			}
+//		}
+//
+//	}
+//
+//	private boolean isSeparator(char c) {
+//		return isBlank(c) || c == '=';
+//	}
+//
+//	static final private String BLANKS = "* \t\n\r";
+//
+//	private boolean isBlank(char c) {
+//		return -1 != BLANKS.indexOf(c);
+//	}
+//
+//	private void setCompletes(boolean tag, boolean attribute) {
+//		m_completesOnTagName = tag;
+//		m_completesOnAttribute = attribute;
+//	}
+//
+//	public String getFragment() {
+//		return m_fragment;
+//	}
+//
+//	public String getTagName() {
+//		return m_tagName;
+//	}
+//
+//	public String getAttributeName() {
+//		return m_attributeName;
+//	}
+//
+//	public boolean completesOnTagName() {
+//		return m_completesOnTagName;
+//	}
+//
+//	public boolean completesOnAttribute() {
+//		return m_completesOnAttribute;
+//	}
+//
+//	private static void ppp(String s) {
+//		System.out.println("[TagFragment] " + s);
+//	}
+//
+//	public static void main(String[] argv) {
+//		// String s = "a=b";
+//		String[] strings = { "", "@ej", "@ej ", "@ej a", "@ej a ", "@ej a =", "@ej a = ", "@ej a = b", "@ej a = \"b\"",
+//				"@ej a = b ", "@ej e = \"a b c\" ", "@ej\n  *  e = \"a b c\"\n  * " };
+//
+//		String[] fragments = { "", "@ej", "", "a", "", "", "", "b", "b", "", "", "" };
+//
+//		boolean[] completesOnAttribute = { false, false, true, true, false, false, false, false, false, true, true,
+//				true };
+//
+//		boolean[] completesOnTagName = { true, true, false, false, false, false, false, false, false, false, false,
+//				false };
+//
+//		String[] attributeNames = { "", "", "", "a", "a", "a", "a", "a", "a", "a", "e", "e" };
+//
+//		for (int i = 0; i < fragments.length; i++) {
+//			ppp("TEST " + i);
+//			ITagFragment tf = TagFragmentFactory.newTagFragment(strings[i]);
+//			assert tf.completesOnTagName() == completesOnTagName[i] : i + " Complete on tagName : found "
+//					+ tf.completesOnTagName() + " but expected " + completesOnTagName[i];
+//			assert tf.getFragment().equals(fragments[i]) : i + " Wrong fragment : found '" + tf.getFragment()
+//					+ "' but expected '" + fragments[i] + "'";
+//			assert tf.completesOnAttribute() == completesOnAttribute[i] : i + " Complete on attribute : found "
+//					+ tf.completesOnAttribute() + " but expected " + completesOnAttribute[i];
+//
+//			String an = tf.getAttributeName() == null ? "" : tf.getAttributeName();
+//			assert an.equals(attributeNames[i]) : i + " Wrong attributeName : found '" + an + "' but expected '"
+//					+ attributeNames[i] + "'";
+//		}
+//
+//		ppp("ALL TESTS PASSED");
+//	}
+//
+//}
